a4846b58565b334c41ced7b2be0ac891dfd54185
Giraphi <hoeps.raphael@gmail.com>
2022-05-04 12:11:43 +0200

    test user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {AppProps} from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        background-color: deeppink;
+    }
+`
+
+const theme = {
+    colors: {
+        primary: '#0070f3',
+    },
 }
 
-export default MyApp
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <GlobalStyle/>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
+}
