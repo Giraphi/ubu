ed183e929ea6192c3400fed19214884cad00fc9d
Giraphi <hoeps.raphael@gmail.com>
2022-05-30 01:38:17 +0200

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,6 +36,27 @@ class MyDocument extends Document {
                         type="font/woff"
                         crossOrigin=""
                     />
+
+                    <meta charSet="utf-8"/>
+                    <link rel="icon" href="%PUBLIC_URL%/icon.ico"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta name="theme-color" content="#000000"/>
+
+                    <meta name="description" content="Ave Ubu. From this point on, there's no turning back, your king awaits you."/>
+                    <meta name="author" content="Ubu Imperator"/>
+                    <meta name="keywords"
+                          content="Ubu Imperator Band Munich Experimental"/>
+
+                    <meta property="og:image" itemProp="image" content="%PUBLIC_URL%/eye.png"/>
+                    <meta property="og:image:type" content="image/jpg"/>
+                    <meta property="og:title" content="Ubu Imperator"/>
+                    <meta property="og:description" content="Ubu Imperator"/>
+                    <meta property="og:type" content="website"/>
+                    <link rel="img_src" href="/preview.png"/>
+                    <link rel="apple-touch-icon" href="/preview.png"/>
+
+                    <title>Ubu Imperator</title>
                 </Head>
                 <body>
                 <Main/>
