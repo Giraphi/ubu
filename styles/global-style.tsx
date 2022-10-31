beea184f6724e6f72956ea0e7c68d687c678d2aa
Giraphi <hoeps.raphael@gmail.com>
2022-10-31 13:42:36 +0100

    font-display

--- styles/global-style.tsx
+++ styles/global-style.tsx
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: "SourceCodePro";
         src: url("/fonts/source-code-pro-regular.woff") format("woff");
+        font-display: swap;
     }
 
     body {
