a8989b6ee898787d3e7a645d794601275e742646
Giraphi <hoeps.raphael@gmail.com>
2022-12-02 16:26:03 +0100

    WIP - translate texts

--- lang/textContent.tsx
+++ lang/textContent.tsx
@@ -2,8 +2,32 @@ export const textContent = {
     home: {
         demo: {
             text: {
-                de: "Ubu präsentiert die ersten frisch fertig gestellten Demo-Aufnahmen. Mit Mastering von Ulrich Treutwein. Vielen Dank!",
+                de: "<a href='google.com'>Ubu</a> präsentiert die ersten frisch fertig gestellten Demo-Aufnahmen. Mit Mastering von Ulrich Treutwein. Vielen Dank!",
                 en: "Ubu presents their first finished demo recordings. Mastered by Ulrich Treutwein. Thanks and cheers!",
+            },
+        },
+        about: {
+            title: {
+                de: "Über uns",
+                en: "About us",
+            },
+            text1: {
+                de: "UBU IMPERATOR ist ein 2022 gegründetes Krautrocktrio aus München mit Genreeinflüssen aus Noise, Experimental und Progressive.",
+                en: "UBU IMPERATOR is a Krautrock Trio founded in 2022 in Munich, influenced by Noise, Experimental and Progressive.",
+            },
+            text2: {
+                de: "Die Musik von UBU IMPERATOR ist meist instrumental und geprägt von stimmungsvoll-krautigen Flächen, immersiven Sequenzen und Arpeggios, sowie teils dystopischem Noise, der von harmonischen Synthesizer Akkorden abgelöst wird.",
+                en: "The music by UBU IMPERATOR is mostly instrumental and shaped by ambient-kraut-ish textures immersive sequences and arpeggios, as well as disruptive noise in alternation with harmonic Synthesizer Chords",
+            },
+            text3: {
+                de: "Der Name der Band bezieht sich auf das Theaterstück „König Ubu” (1896) des französischen Schriftstellers Alfred Jarry. Ein Stück, das wegen seines grotesken, karnevalesken und absurden Charakters viele surrealistische und dadaistische Künstler der damaligen Zeit in Erstaunen versetzte. Ubu Imperator ist auch der Titel eines der bekanntesten Werke von Max Ernst.",
+                en: "The band's name is a reference to the theater piece ”King Ubu” (1896) by the french writer Alfred Jarry. Because of its grotesque, carnivalesque and absurd character the piece astonished many surreal and dadistic artists of that time. Ubu Imperator is also the title of one of the most famous works by Max Ernst.",
+            },
+        },
+        video: {
+            text: {
+                de: "Madness! Ubu Drummer und Illustrator Daniel hat die Nächte durchgezeichnet und ein irrwitziges Delirium von einem Musikvideo zu unserem Song Kardinalkardinal erschaffen. Hope you like it!",
+                en: "Madness! Ubu drummer and illustrator Daniel was spending multiple nights with drawing and fabricated an insane delirium of a music video for our song kardinalkardinal. Viel Spaß!",
             },
         },
     },
