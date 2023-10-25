d4eb5380a7be6d47c92c9468e7b7df5d31ce7f5a
Giraphi <hoeps.raphael@gmail.com>
2023-10-25 10:48:18 +0200

    counter logic

--- components/StackedPhotos/StackedPhotos.tsx
+++ components/StackedPhotos/StackedPhotos.tsx
@@ -12,36 +12,37 @@ import rebl from "../../images/pile/rebl.jpg";
 import gallery from "../../images/pile/gallery.jpg";
 
 import StackedPhoto from "./StackedPhoto";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RotateButton from "../RotateButton";
+import { shuffleArray } from "../../util/functions";
 
 export interface PicturePileProps {}
 
 export default function StackedPhotos(props: PicturePileProps) {
-    const [pileImageProps] = useState([
-        { src: kaufbeurenLogo, text: "11.03.22 Roundhouse, Kaufbeuren", textRight: true },
-        { src: UBU3, text: "Bandportrait by Vanessa Mönius" },
-        { src: ruebeSimon, text: "2022 Ruebezahl Festival" },
-        { src: rebl, text: "24.04.23 Walter Storms Galerie, München" },
-        { src: gallery, text: "24.04.23 Walter Storms Galerie, München" },
-        { src: balveDaniel, text: "27.05.2023 Kunstblock Balve, München" },
-        { src: balveDrums, text: "27.05.2023 Kunstblock Balve, München" },
-        { src: kaufbeurenBand, text: "11.03.22 Roundhouse, Kaufbeuren" },
-        { src: UBU2, text: "Bandportrait by Vanessa Mönius" },
-        { src: murf1, text: "01.10.23 Murf/Murw Festival, Tilburg NL" },
-        { src: murf3, text: "30.09.23 Murf/Murw Festival, Tilburg NL" },
-        { src: murf2, text: "30.09.23 Murf/Murw Festival, Tilburg NL" },
+    const [pileImageProps, setPileImageProps] = useState([
+        { src: kaufbeurenLogo, text: "11.03.22 Roundhouse, Kaufbeuren", textRight: true, id: "1" },
+        { src: UBU3, text: "Bandportrait by Vanessa Mönius", id: "2" },
+        { src: ruebeSimon, text: "2022 Ruebezahl Festival", id: "3" },
+        { src: rebl, text: "24.04.23 Walter Storms Galerie, München", id: "4" },
+        { src: gallery, text: "24.04.23 Walter Storms Galerie, München", id: "5" },
+        { src: balveDaniel, text: "27.05.2023 Kunstblock Balve, München", id: "6" },
+        { src: balveDrums, text: "27.05.2023 Kunstblock Balve, München", id: "7" },
+        { src: kaufbeurenBand, text: "11.03.22 Roundhouse, Kaufbeuren", id: "8" },
+        { src: UBU2, text: "Bandportrait by Vanessa Mönius", id: "9" },
+        { src: murf1, text: "01.10.23 Murf/Murw Festival, Tilburg NL", id: "10" },
+        { src: murf3, text: "30.09.23 Murf/Murw Festival, Tilburg NL", id: "11" },
+        { src: murf2, text: "30.09.23 Murf/Murw Festival, Tilburg NL", id: "12" },
     ]);
 
-    const [showImages, setShowImages] = useState(pileImageProps.length);
+    const [counter, setCounter] = useState(pileImageProps.length);
 
     function handleClick() {
-        if (showImages === 0) {
-            setShowImages(pileImageProps.length);
+        if (counter === -1 * pileImageProps.length) {
+            setCounter(pileImageProps.length - 1);
             return;
         }
 
-        setShowImages((x) => x - 1);
+        setCounter((x) => x - 1);
     }
 
     return (
@@ -53,9 +54,9 @@ export default function StackedPhotos(props: PicturePileProps) {
                 <div className={"relative h-full w-full select-none"}>
                     {pileImageProps.map((props, index) => (
                         <StackedPhoto
-                            key={index}
+                            key={props.id}
                             index={index}
-                            showImages={showImages}
+                            showImages={Math.abs(counter)}
                             {...props}
                         />
                     ))}
