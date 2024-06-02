b89bb99a1a7ebc9974070d8d749de7f8d4debd4f
Giraphi <hoeps.raphael@gmail.com>
2024-06-02 11:47:09 +0200

    feat: background svg

--- components/sections/Fruhtrunk/FruhtrunkBackground.tsx
+++ components/sections/Fruhtrunk/FruhtrunkBackground.tsx
@@ -1,6 +1,8 @@
 import React, { FC, ReactNode, RefObject } from "react";
 import { Device, useDeviceStore } from "../../../context/useDeviceStore";
 import FruhtrunkBackgroundLg from "./FruhtrunkBackgroundLg";
+import Lines from "../../../images/lines.svg";
+import Image from "next/image";
 
 export type FruhtrunkBackgroundProps = {
     children: ReactNode;
@@ -13,6 +15,25 @@ export default function FruhtrunkBackground(props: FruhtrunkBackgroundProps) {
     return device > Device.md ? (
         <FruhtrunkBackgroundLg scrollRef={props.scrollRef}>{props.children}</FruhtrunkBackgroundLg>
     ) : (
-        <div>{props.children}</div>
+        <div className="relative">
+            <div className="absolute left-0 top-0 flex size-full items-center justify-center overflow-hidden">
+                {/* 
+                currently I didn not find a way to set the <Lines> to object-fit:cover
+                The current approach with height:100% plus overflow-hidden on parent assumes that the svg
+                canvas is wide enough to cover the mobile
+                 */}
+                <Lines
+                    style={{
+                        position: "absolute",
+                        height: "150vh",
+                        width: "auto",
+                        right: "50%",
+                        transform: "translateX(50%) translateY(-50%) rotate(45deg)",
+                        top: "50%",
+                    }}
+                />
+            </div>
+            <div className="relative z-10 pb-8 pt-3">{props.children}</div>
+        </div>
     );
 }
